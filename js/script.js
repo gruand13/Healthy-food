fe29e4e75b3f5700a41eee8ade17722d13992b04
Andrey Hrusheuski <gruand13@gmail.com>
2022-06-05 10:32:18 +0300

    lesson 85 user notification with spinner

--- js/script.js
+++ js/script.js
@@ -118,8 +118,8 @@ setClock('.timer', deadline);
 // Modal 
 
 const   modalTrigger= document.querySelectorAll('[data-modal]'),
-        modal=document.querySelector('.modal'),
-        modalCloseBtn=document.querySelector('[data-close]');
+        modal=document.querySelector('.modal');
+        
 
         function openModal(){
             modal.classList.add('show');
@@ -139,10 +139,10 @@ const   modalTrigger= document.querySelectorAll('[data-modal]'),
             document.body.style.overflow ='';
 
             }
-            modalCloseBtn.addEventListener('click', closeModal);
+            
 
             modal.addEventListener('click', (e)=>{
-                if (e.target==modal){
+                if (e.target==modal || e.target.getAttribute('data-close')==''){
                     closeModal();
                 }
             });
@@ -151,7 +151,7 @@ const   modalTrigger= document.querySelectorAll('[data-modal]'),
                     closeModal();
                 }
             });
-            // const modalTimerId = setTimeout(openModal, 2000);
+            const modalTimerId = setTimeout(openModal, 50000);
 
             function showModalByScroll(){
                 if (window.pageYOffset+  document.documentElement.clientHeight>= document.documentElement.scrollHeight-1 ){
@@ -234,7 +234,7 @@ const   modalTrigger= document.querySelectorAll('[data-modal]'),
 // forms
         const forms = document.querySelectorAll('form');
         const message ={
-            loading: 'Загрузка',
+            loading: 'img/form/spinner.svg',
             success: 'Спасибо! мы с вами свяжемся',
             failure: 'Что-то пошло не так...'
         };
@@ -247,10 +247,14 @@ const   modalTrigger= document.querySelectorAll('[data-modal]'),
             form.addEventListener('submit', (e)=>{
                 e.preventDefault();
 
-                const statusMessage= document.createElement('div');
-                statusMessage.classList.add('status');
-                statusMessage.textContent = message.loading;
-                form.append(statusMessage);
+                const statusMessage= document.createElement('img');
+                statusMessage.src = message.loading;
+                statusMessage.style.cssText =`
+                display: block;
+                margin: 0 auto;
+                `;
+               
+                form.insertAdjacentElement('afterend', statusMessage);
 
                 const request = new XMLHttpRequest();
                 request.open('POST', 'server.php');
@@ -268,13 +272,12 @@ const   modalTrigger= document.querySelectorAll('[data-modal]'),
                 request.addEventListener('load', ()=>{
                     if (request.status === 200){
                         console.log(request.response);
-                        statusMessage.textContent = message.success;
+                        showThanksModal(message.success);
                         form.reset(); //сбрасываем форму
-                        setTimeout(()=> {
-                            statusMessage.remove();
-                        }, 2000); // удаляем надпись через 2 сек
+                        statusMessage.remove();
+                        // удаляем надпись через 2 сек
                     } else {
-                        statusMessage.textContent = message.failure;
+                        showThanksModal(message.failure);
                     }
                 });
 
@@ -282,6 +285,31 @@ const   modalTrigger= document.querySelectorAll('[data-modal]'),
             });
         }
 
+        function showThanksModal (message) {
+            const prevModalDialog = document.querySelector('.modal__dialog');
+            prevModalDialog.classList.add('hide');
+            openModal();
+
+            const thanksModal = document.createElement('div');
+            thanksModal.classList.add('modal__dialog');
+            thanksModal.innerHTML = `
+            <div class= "modal__content">
+                <div class ="modal__close" data-close>&times;</div>
+                <div class ="modal__title">${message}</div>
+            
+            
+            </div>
+            `;
+            document.querySelector(".modal").append(thanksModal);
+            setTimeout(()=>{
+                thanksModal.remove();
+                prevModalDialog.classList.add('show');
+                prevModalDialog.classList.remove('hide');
+                closeModal();
+
+            }, 4000);
+
+        }
 });
 
 
