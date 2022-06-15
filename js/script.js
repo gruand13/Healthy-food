00785f39ff1c2a076e351a4cd729fa9be0767f7b
Andrey <gruand13@gmail.com>
2022-06-15 10:55:12 +0300

    move regular expression to the function lesson 96

--- js/script.js
+++ js/script.js
@@ -425,11 +425,15 @@ const   modalTrigger= document.querySelectorAll('[data-modal]'),
                 dots[slideIndex-1].style.opacity = 1;
 
             };
+
+            function deleteNotDigits(str){
+                return +str.replace(/\D/g , '');
+            };
             next.addEventListener('click', ()=>{
-                if (offset == +width.replace(/\D/g , '') * (slides.length-1)){
+                if (offset == deleteNotDigits(width) * (slides.length-1)){
                     offset=0;
                 } else{
-                    offset+= +width.replace(/\D/g , '') ;
+                    offset+= deleteNotDigits(width) ;
                 }
                 slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -450,9 +454,9 @@ const   modalTrigger= document.querySelectorAll('[data-modal]'),
              prev.addEventListener('click', ()=>{
                 if (offset==0){
                     
-                    offset = +width.replace(/\D/g , '') * (slides.length-1);
+                    offset = deleteNotDigits(width) * (slides.length-1);
                 } else{
-                    offset -= +width.replace(/\D/g , '') ;
+                    offset -= deleteNotDigits(width) ;
                 }
                 slidesField.style.transform = `translateX(-${offset}px)`;
 
